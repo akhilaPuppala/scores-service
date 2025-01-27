b374c2c9772d24ebc43091351db00af62376fc34
vishrutth5214 <vishrutth5214@gmail.com>
2025-01-27 09:19:08 +0530

    created

---
+++ src/main/java/com/cricket/scores_service/ScoresServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cricket.scores_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoresServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoresServiceApplication.class, args);
+	}
+
+}
