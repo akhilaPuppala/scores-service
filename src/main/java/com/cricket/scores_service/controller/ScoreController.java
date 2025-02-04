6b0d4f42b6f8cb6053a2df3f8b5d75152bb9e566
vishrutth5214 <vishrutth5214@gmail.com>
2025-02-04 11:00:30 +0530

    gfgf

--- src/main/java/com/cricket/scores_service/controller/ScoreController.java
+++ src/main/java/com/cricket/scores_service/controller/ScoreController.java
@@ -7,14 +7,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cricket.scores_service.dto.ScoresPojo;
 import com.cricket.scores_service.entity.Scores;
 import com.cricket.scores_service.service.ScoresService;
 
-@RequestMapping
+@RequestMapping("/scores")
 @RestController
 public class ScoreController {
     @Autowired
@@ -32,5 +34,9 @@ public class ScoreController {
     @GetMapping
     public ResponseEntity<List<Scores>> getAllScores(){
         return new ResponseEntity<List<Scores>>(scoresService.getAllScores(),HttpStatus.OK);
+    }
+    @PutMapping("/update")
+    public ResponseEntity<Scores> updateScores(@RequestBody Scores updateScore){
+    	return new ResponseEntity<Scores>(scoresService.updateScores(updateScore),HttpStatus.OK);
     }
 }
--- src/main/java/com/cricket/scores_service/service/ScoresService.java
+++ src/main/java/com/cricket/scores_service/service/ScoresService.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import com.cricket.scores_service.dto.LiveScore;
@@ -22,6 +20,27 @@ public class ScoresService {
     @Autowired
     ScoreRepository scoreRepository;
 
+    public Scores updateScores(Scores updateScores) {
+        // Find the existing score by ID
+        Scores existingScore = scoreRepository.findById(updateScores.getId()).orElse(null);
+
+        if (existingScore == null) {
+            throw new RuntimeException("Score with ID " + updateScores.getId() + " not found");
+        }
+
+        // Update only the necessary fields
+        existingScore.setBowlerId(updateScores.getBowlerId());
+        existingScore.setGameBallNo(updateScores.getGameBallNo());
+        existingScore.setGameTotalOvers(updateScores.getGameTotalOvers());
+        existingScore.setGameWickets(updateScores.getGameWickets());
+        existingScore.setNonStrikerPlayerId(updateScores.getNonStrikerPlayerId());
+        existingScore.setStrikerPlayerId(updateScores.getStrikerPlayerId());
+        existingScore.setStrikerRuns(updateScores.getStrikerRuns());
+        existingScore.setWideBall(updateScores.getWideBall());
+
+        // Save the updated entity
+        return scoreRepository.save(existingScore);
+    }
     public Scores createScores(Scores score){
         return scoreRepository.save(score);
     }
